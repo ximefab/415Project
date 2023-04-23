95423f91eb9067af8b022e8ceb76e69052bc166a
Ximena Fabian <ximenafabian@outlook.com>
2023-04-22 22:35:02 -0500

    FINALLY DONE

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 const path = require('path');
+const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
+
+//app.get/post and /update link the html forms to the route /post and /update
 
 app.get('/Post', function(req, res) {
   res.sendFile(path.join(__dirname + '/first.html'));
@@ -17,161 +21,178 @@ app.get('/Update', function(req, res) {
 });
 
 
-// read the content/data of tickets.json file
-const ticketsData = fs.readFileSync('./tickets.json');
+// const Ticket = require('../models/ticket');
 
-// parse JSON data
-const tickets = JSON.parse(ticketsData);
+const uri = 'mongodb+srv://User1:CxA5WyB3MGfxyrQ8@ximfab415.pisguor.mongodb.net/';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    const db = client.db('ximfab415');
+    return db;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const dbPromise = connectToDatabase();
 
 
-// Get APIs
-// Api to get a list of the tickets
-app.get('/rest/list/', (req, res) => {
-    //error check, if json file is empty then the text "there are no tickets" gets returned to the console.
+//Here you get the ticket list
+app.get('/rest/list', async (req, res) => {
+  try {
+    const db = await dbPromise;
+    const collection = db.collection('415');
+    const tickets = await collection.find().toArray();
     if (tickets.length === 0) {
-        return res.status(404).send("There are no tickets!");
-      }
-
-    // send tickets data
+      return res.status(404).send('There are no tickets!');
+    }
     console.log(tickets);
     res.send(tickets);
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
 
-//Api to get a single ticket
-app.get('/rest/ticket/:id', (req, res) => {
-    //method to find the ticket based on the id 
-    const ticket = tickets.find((x) => x.id === Number(req.params.id));
-
-    //If the input id does not exist, "ticket not found" is printed to the console
+//here you get a ticket by id from the mongodb!
+app.get('/rest/ticket/:id', async (req, res) => {
+  try {
+    const db = await dbPromise;
+    const collection = db.collection('415');
+    const ticket = await collection.findOne({ _id: Number(req.params.id) });
     if (!ticket) {
-        return res.status(404).send("Ticket not found. Make sure you put the right id number!");
+      return res.status(404).send('Ticket not found. Make sure you put the right id number!');
+    }
+    console.log(ticket);
+    res.send(ticket);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+//here you delete a ticket from the db!
+app.delete('/rest/ticket/:id', async (req, res) => {
+  try {
+    const db = await dbPromise;
+    const collection = db.collection('415');
+    const result = await collection.deleteOne({ _id: Number(req.params.id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).send('Ticket not found');
     }
 
-    //send single ticket data
-    console.log(ticket) 
-    res.send(ticket);
-})
+    console.log('Ticket deleted successfully');
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
 
 
 //Post API to make a new ticket
 app.use(express.json());
-app.post('/rest/maketicket', (req, res) => {
-    const { id, type, subject, description, priority, status, recipient, submitter, assignee_id, follower_ids, tags } = req.body;
-    
-    //Error checking for invalid subject, description, priority, status, recipient, submitter, or tags missing. 
-    if (!subject) {
-      return res.status(400).send("Error! Missing subject field.");
-    }
-    if (!type) {
-      return res.status(400).send("Error! Missing type field.");
-    }
-    if (!description) {
-      return res.status(400).send("Error! Missing description field.");
-    }
-    if (!priority) {
-      return res.status(400).send("Error! Missing priority field.");
-    }
-    if (!status) {
-      return res.status(400).send("Error! Missing status field.");
-    }
-    if (!recipient) {
-      return res.status(400).send("Error! Missing recipient field.");
-    }
-    if (!submitter) {
-      return res.status(400).send("Error! Missing submitter field.");
+const ticketSchema = new mongoose.Schema({
+  _id: Number,
+  type: String,
+  subject: String,
+  description: String,
+  priority: String,
+  status: String,
+  recipient: String,
+  submitter: String,
+  assignee_id: Number,
+  follower_ids: [Number]
+}, { timestamps: true });
+
+// Create the Ticket model using the schema
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+// Create a new ticket and save it to the database
+app.post('/rest/maketicket', async (req, res) => {
+  const { id, type, subject, description, priority, status, recipient, submitter, assignee_id, follower_ids } = req.body;
+
+  // Error checking for missing fields
+  if (!subject || !type || !description || !priority || !status || !recipient || !submitter) {
+    return res.status(400).send('Missing required fields');
+  }
+
+  // Create a new ticket object using the request body
+  const newTicket = {
+    _id: parseInt(id),
+    type,
+    subject,
+    description,
+    priority,
+    status,
+    recipient,
+    submitter,
+    assignee_id,
+    follower_ids,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  // Insert the new ticket into the database
+  try {
+    const db = await dbPromise;
+    const collection = db.collection('415');
+    await collection.insertOne(newTicket);
+    res.status(201).send(newTicket);
+  } catch (err) {
+    res.status(500).send(`Error creating ticket: ${err}`);
+  }
+});
+
+//update tickets!
+app.put('/rest/ticket/:id', async (req, res) => {
+  try {
+    const db = await dbPromise;
+    const collection = db.collection('415');
+
+    // Find the existing ticket by ID
+    const existingTicket = await collection.findOne({ _id: Number(req.params.id) });
+
+    // If the ticket doesn't exist, return 404 error
+    if (!existingTicket) {
+      return res.status(404).send('Ticket not found. Make sure you put the right id number!');
     }
 
-    
-      //Error checking to make sure the assignee and follower dont have duplicating ids, sends prompt that the id already exist if thats the case for either of them
-    const existingAssignee = tickets.find(ticket => ticket.assignee_id === assignee_id);
-    const existingFollower = tickets.find(ticket => ticket.follower_ids.includes(follower_ids));
-    if (existingAssignee || existingFollower) {
-        return res.status(400).send('Error! Assignee or follower Id already exists.');
-    }
-
-    //sets the id to whatever number is input, if it is null, then it just makes the id the length + 1, but if the id already exists it does not let user add that as id
-
-    let newId;
-    
-    if (!id || id==null) {
-      newId = tickets.length + 1;
-    } else {
-      newId = Number(id);
-      const ticketWithSameId = tickets.find((ticket) => ticket.id === newId);
-      if (ticketWithSameId) {
-        return res.status(409).send("A ticket with the same id already exists! Choose a new id number. ");
-      }
-    }
-    
-    //setting variables etc
-    const newTicket = {
-      id: newId,
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-      type: type,
-      subject: subject,
-      description: description,
-      priority: priority,
-      status: status,
-      recipient: recipient,
-      submitter: submitter,
-      assignee_id: assignee_id,
-      follower_ids: follower_ids,
-      tags: tags
+    // Replace the existing ticket with the new ticket object from the request body
+    const { type, subject, description, priority, status, recipient, submitter, assignee_id, follower_ids } = req.body;
+    const updatedTicket = {
+      _id: existingTicket._id,
+      type: type || existingTicket.type,
+      subject: subject || existingTicket.subject,
+      description: description || existingTicket.description,
+      priority: priority || existingTicket.priority,
+      status: status || existingTicket.status,
+      recipient: recipient || existingTicket.recipient,
+      submitter: submitter || existingTicket.submitter,
+      assignee_id: assignee_id || existingTicket.assignee_id,
+      follower_ids: follower_ids || existingTicket.follower_ids,
+      createdAt: existingTicket.createdAt,
+      updatedAt: new Date(),
     };
-    
-    //pushes tickets into newTicket
-    tickets.push(newTicket);
-    
-    // write the updated tickets array into the tickets.json file
-    fs.writeFileSync('./tickets.json', JSON.stringify(tickets, null, 2));
-    
-    //return the status in postman
-    res.status(201).json(newTicket);
-  });
+    const result = await collection.replaceOne({ _id: Number(req.params.id) }, updatedTicket);
 
-  // DELETE API to delete a single ticket by ID
-app.delete('/rest/ticket/:id', (req, res) => {
-  // Find the index of the ticket with the given ID
-  const index = tickets.findIndex((ticket) => ticket.id === Number(req.params.id));
-
-  // If no ticket is found with the given ID, return a 404 error
-  if (index === -1) {
-    return res.status(404).send('Ticket not found');
+    // If the ticket was successfully replaced, return the updated ticket object
+    if (result.modifiedCount === 1) {
+      console.log('Ticket replaced successfully');
+      res.send(updatedTicket);
+    } else {
+      res.status(500).send('Error replacing ticket');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
   }
-
-  // Remove the ticket from the tickets array
-  const deletedTicket = tickets.splice(index, 1)[0];
-
-  // Write the updated tickets array into the tickets.json file
-  fs.writeFileSync('./tickets.json', JSON.stringify(tickets, null, 2));
-
-  // Print "Deleted successfully!" to the console
-  console.log("Deleted successfully!");
-
-  // Return the deleted ticket as JSON
-  res.status(200).json(deletedTicket);
 });
 
-
-
-// PUT API to update a single ticket by ID
-app.put('/rest/ticket/:id', (req, res) => {
-  const index = tickets.findIndex((ticket) => ticket.id === Number(req.params.id));
-
-  // If no ticket is found with the given ID, return a 404 error
-  if (index === -1) {
-    return res.status(404).send('Ticket not found');
-  }
-
-  // Replace the ticket with the updated ticket
-  const updatedTicket = { ...tickets[index], ...req.body };
-  tickets[index] = updatedTicket;
-
-
-  fs.writeFileSync('./tickets.json', JSON.stringify(tickets, null, 2));
-  res.status(200).json(updatedTicket);
-});
 
 app.listen(3000, ()=>{
     console.log(`Node API app is running on port 3000`)
